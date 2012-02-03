b7f1775a33c251573e225fb0263b13cf01983fef
Brian Peraza <brian.peraza@gmail.com>
2012-02-02 17:44:25 -0800

    Edited html in pages and modified pagescontroller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
   def about
+    @title ="About"
   end
 
   def contact
+    @title = "Contact"
   end
 
   def home
+    @title = "Home"
   end
 
   def rates
+    @title = "Rates"
   end
 
   def services
+    @title = "Services"
   end
 
 end
